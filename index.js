941b0b6727f57f5ed34802cab5c8cf32a893412f
Wong Jin Xuan <jinxuan03@gmail.com>
2024-06-13 16:01:52 +0800

    Hash Table

--- index.js
+++ index.js
@@ -28,6 +28,55 @@ class Stack {
   }
 }
 
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.hash_table = new Array(size).fill(null).map(() => []);
+  }
+
+  hash(key){
+    return key.toUpperCase().charCodeAt(0) - 65;
+  }
+
+  set(key, value){
+    const index = this.hash(key);
+    const bucket = this.hash_table[index];
+
+    for(var i = 0; i < bucket.length; i++) {
+      if(bucket[i][0] == key) {
+        bucket[i][1] = value;
+        return;
+      }
+    }
+    bucket.push([key, value]);
+  }
+
+  get(key){
+    const index = this.hash(key);
+    const bucket = this.hash_table[index];
+
+    for(var i = 0; i < bucket.length; i++) {
+      if(bucket[i][0] == key) {
+        return bucket[i][1];
+      }
+    }
+    return undefined;
+  }
+
+  delete(key){
+      const index = this.hash(key);
+      const bucket = this.hash_table[index];
+
+      for(var i = 0; i < bucket.length; i++) {
+        if(bucket[i][0] == key) {
+          bucket.splice(i, 1);
+          return true;
+        }
+      }
+      return false;
+  }
+}
+
 // Requiring Readline Module to retrieve user input
 // https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
 const readline = require("node:readline");
@@ -40,12 +89,18 @@ const rl = readline.createInterface({
 // Main function to run the program
 function main() {
   // Prompt for user input
-  rl.question("[ PostFix++ Calculator ]\nPlease Enter a PostFix++ Expression. For Example: 3 4 5 + *\nYour Input: ", (input_array) => {
+  rl.question("--------------\n[ PostFix++ Calculator ]\n--------------\nList of commands:\n1. Enter a PostFix++ Expression. For Example: 3 4 5 + *\n2. 'search <variable>' - Search for the value of a variable.\n3. 'set <variable> <value>' - Set a value to a variable.\n4. 'delete <variable>' - Remove a variable and its value\n--------------\nYour Input: ", (input_array) => {
       // Split the input into an array when a space is detected
       input_array = input_array.split(" ");
-      console.log("Steps breakdown: ");
-      // Show the output of the PostFix function in the console using the user input
-      console.log("Final Output:", postFix(input_array) ? postFix(input_array) : "Updated variable successfully.", "\n");;
+
+      if(!checkInput(input_array)){
+        console.log("Steps breakdown: ");
+        // Show the output of the PostFix function in the console using the user input
+        console.log("Final Output:", postFix(input_array), "\n");;
+        } else {
+          checkInput(input_array);
+        }
+        
       // Run the main function again to allow for more user input
       main();
     }
@@ -56,6 +111,48 @@ main();
 
 // Create an array to store variables and their values
 const variables = [];
+
+const hashTable = new HashTable(26);
+
+function checkInput(input) {
+  if (input.length >= 3 && input[0].toUpperCase() === "SET") {
+    // Set command: set a 2
+    const variable = input[1].toUpperCase();
+    const value = input.slice(2).join(" "); // Join all elements from index 2 onwards as the value
+
+    hashTable.set(variable, parseFloat(value));
+    console.log(`Variable [${variable}] has been set to ${value}.`);
+    return true;
+  } else if (input.length === 2 && input[0].toUpperCase() === "SEARCH") {
+    // Search command: search a
+    const variable = input[1].toUpperCase();
+    const value = hashTable.get(variable);
+    if (value !== undefined) {
+      console.log(`Searching for variable: ${variable}`);
+      console.log(`Value: ${value}`);
+      return true; // Return true after logging the search result
+    } else {
+      console.log(`Variable ${variable} not found.`);
+      return false; // Return false if variable is not found
+    }
+  } else if (input.length === 2 && input[0].toUpperCase() === "DELETE") {
+    // Delete command: delete a
+    const variable = input[1].toUpperCase();
+    const deleted = hashTable.delete(variable);
+    if (deleted) {
+      console.log(`Deleting variable: ${variable}`);
+      return true; // Return true after deleting the variable
+    } else {
+      if(!deleted){
+        console.log(`Variable ${variable} not found.`);
+      }
+      return false; // Return false if variable is not found
+    }
+  } else {
+    console.log("Invalid command. Please use the following commands: set <variable> <value>, search <variable>, delete <variable>");
+    return false; // Return false for invalid commands
+  }
+}
 
 function postFix(array) {
   // Create a new stack
@@ -74,7 +171,7 @@ function postFix(array) {
       // Check if the element is an alphabet
       else if (element.match(/[a-zA-Z]/)) {
         // Convert the alphabet variable to uppercase
-        element.toUpperCase();
+        element = element.toUpperCase();
         // Push the alphabet variable to the stack
         stack.push(element);
     } else {
@@ -187,11 +284,15 @@ function postFix(array) {
           // Check if the element is a variable
           if (isNaN(x)) {
             // Set the value of the variable to the value of the other variable
-            variables[y] = variables[x];
-            console.log("Variable [", y.toUpperCase(),"] has been set to ", variables[x] + ".");
+            // variables[y] = variables[x];
+            // console.log("Variable [", y.toUpperCase(),"] has been set to ", variables[x] + ".");
+            hashTable.set(y, hashTable.get(x));
+            console.log("Variable [", y.toUpperCase(),"] has been set to ", hashTable.get(x) + ".");
           } else {
             // Set the value of the variable to the number as a float
-            variables[y] = parseFloat(x);
+            // variables[y] = parseFloat(x);
+            // console.log("Variable [", y.toUpperCase(),"] has been set to ", x + ".");
+            hashTable.set(y, parseFloat(x));
             console.log("Variable [", y.toUpperCase(),"] has been set to ", x + ".");
           }
           break;
@@ -214,7 +315,8 @@ function getVarValue(element) {
   // Check if the element is a number
   if (isNaN(element)) {
     // Return the value of the variable if it is a variable
-    return variables[element];
+    // return variables[element];
+    return hashTable.get(element);
   } else {
     // Convert the element to a float if it is a number and return it
     return parseFloat(element);
